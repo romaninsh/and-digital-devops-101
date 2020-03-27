a4a65cbf92e3c98e3f98c4cc29edb6406e7be97c
Romans Malinovskis <me@nearly.guru>
2020-03-27 10:10:53 +0000

    add nyakpo

--- dns.tf
+++ dns.tf
@@ -7,18 +7,25 @@ resource "aws_route53_zone" "dns" {
 
 locals {
   subdomains = {
-    "uat-marius.aa.dekker-and.digital" : [
+    "uat-marius" : [
       "ns-1010.awsdns-62.net",
       "ns-1067.awsdns-05.org",
       "ns-1589.awsdns-06.co.uk",
       "ns-67.awsdns-08.com",
     ],
 
-    "uat-andrea.aa.dekker-and.digital" : [
+    "uat-andrea" : [
       "ns-89.awsdns-11.com",
       "ns-1968.awsdns-54.co.uk",
       "ns-926.awsdns-51.net",
       "ns-1506.awsdns-60.org",
+    ],
+
+    "nyakpo" : [
+      "ns-888.awsdns-47.net",
+      "ns-1572.awsdns-04.co.uk",
+      "ns-1481.awsdns-57.org",
+      "ns-266.awsdns-33.com",
     ]
   }
 }
@@ -30,6 +37,6 @@ resource "aws_route53_record" "rec" {
 
   for_each = local.subdomains
 
-  name = each.key
+  name = "${each.key}.aa.dekker-and.digital"
   records = each.value
 }
